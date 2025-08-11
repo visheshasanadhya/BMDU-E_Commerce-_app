132c73cd7c07392f50210ceac50621f7d0ef56fc
vishesha <sanadhya2003@gmail.com>
2025-08-11 07:51:46 +0530

    Deploy web build to gh-pages

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "b7a729cd06d97d7e82aebf7c364b7a30",
+const RESOURCES = {"flutter_bootstrap.js": "d4e927735228e0cd2f7d8c7c05c7eb16",
 "version.json": "d78f5551c6ab7d116c8aad74114b9ba9",
 "index.html": "5c516a612b844c4319ea011ca9c1bf46",
 "/": "5c516a612b844c4319ea011ca9c1bf46",
